19b76a4f0333fa3aab5a2a59f1ee6efff62fbb2e
maplestorykidd <34246540+maplestorykidd@users.noreply.github.com>
2018-06-26 14:49:10 +0800

    添加项目文件。

---
+++ Hi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hi.Startup))]
+namespace Hi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
